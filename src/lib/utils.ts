0228aa640c9e0e75d18c038fa02520699448ae7e
3two6 <hiccup.dev@outlook.com>
2024-07-23 13:57:28 -0600

    fix: my activity

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import moment from 'moment';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function addDaysToTimestamp(timestamp: string, days: number) {
-  // Create a Date object from the timestamp
   const originalDate = new Date(timestamp);
+  originalDate.setDate(originalDate.getDate() + days);
+  return originalDate.toISOString();
+}
 
-  // Add the specified number of days to the date
-  const newDate = new Date(originalDate);
-  newDate.setDate(originalDate.getDate() + days);
+export function formatTimeStamp(timestamp: string) {
+  return moment(timestamp).format('DD/MM/YYYY, HH:mm');
+}
 
-  // Format the resulting date as ISO string
-  return newDate.toISOString();
+export function formatTxHash(txHash: string) {
+  if (txHash.length <= 10) return txHash;
+  return `${txHash.slice(0, 10)}...${txHash.slice(-10)}`;
 }
