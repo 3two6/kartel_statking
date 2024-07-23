e18d44c5e0c5714b16976f1289a323d58c8a1ae5
caraX0 <iBranch211@outlook.com>
2024-07-23 08:16:16 +0200

    feat: add staking api

--- src/constant/token.ts
+++ src/constant/token.ts
@@ -1,8 +1,14 @@
+const isProduction = process.env.NEXT_PUBLIC_NETWORK === "mainnet";
+
 export const KART_DENOM =
-  "factory/kujira1sr9xfmzc8yy5gz00epspscxl0zu7ny02gv94rx/kartel";
+  isProduction
+    ? "factory/kujira13x2l25mpkhwnwcwdzzd34cr8fyht9jlj7xu9g4uffe36g3fmln8qkvm3qn/ukart"
+    : "factory/kujira1sr9xfmzc8yy5gz00epspscxl0zu7ny02gv94rx/kartel";
 
 export const USK_DENOM =
-  "factory/kujira1sr9xfmzc8yy5gz00epspscxl0zu7ny02gv94rx/kartelUSk";
+  isProduction
+    ? "factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk"
+    : "factory/kujira1sr9xfmzc8yy5gz00epspscxl0zu7ny02gv94rx/kartelUSk";
 
 export const STAKING_ADDR =
   "kujira1s9kqaep4mwpufcu72hdv9a6ztsqmwuxfjhdmuuufhh590k702lasj6u47d";
