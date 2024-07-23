08d15258d8491debbc19ad753e0f9c12a92638ca
3two6 <hiccup.dev@outlook.com>
2024-07-23 09:30:32 -0600

    fix: spinner icon

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,6 +1,5 @@
 import { useAppState } from "@/store/app.store"
 import Spinner from "./spinner"
-import Image from "next/image"
 
 function Loading() {
     const appState = useAppState()
@@ -8,13 +7,6 @@ function Loading() {
         return (
             <div className='flex absolute w-full h-full justify-center items-center backdrop-blur-sm bg-[#0000004D] z-30'>
                 <Spinner />
-                <Image
-                    src="/images/logo.png"
-                    width={100}
-                    height={100}
-                    className="absolute w-10 h-10"
-                    alt="loading"
-                />
             </div>
         )
     else return null
