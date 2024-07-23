e0b5667d9e68374fca7c762ffe7ff324150d7471
3two6 <hiccup.dev@outlook.com>
2024-07-23 10:41:15 -0600

    fix: staking card

--- src/section/staking/index.tsx
+++ src/section/staking/index.tsx
@@ -106,73 +106,80 @@ export default function StakingSection() {
       </main>
       <KartCard className="my-10 flex w-full max-w-lg flex-col items-start gap-y-8 p-8 drop-shadow-16 sm:mb-0">
         <h1 className="text-gray-300 font-normal">KART STAKING</h1>
-        <div className="flex w-full space-x-2 rounded-sm bg-transparent p-2 transition-all duration-200 ease-in-out">
+        <div className="flex w-full space-x-2 rounded-sm bg-transparent p-2 transition-all duration-200 ease-in-out border border-purple-border">
           {StakingOptions.map((item, index) => (
             <button
               key={index}
-              className={`w-full rounded-sm py-2 border transition-all duration-200 ease-in-out bg-purple text-gray-300 ${selectedOption === item.value ? "bg-purple border-transparent" : "bg-transparent border-purple-border"}`}
+              className={`w-full rounded-sm py-2 border transition-all duration-200 ease-in-out bg-purple text-gray-300 ${selectedOption === item.value ? "bg-purple border-transparent" : "bg-transparent border-transparent"}`}
               onClick={() => setSelectedOption(item.value)}
             >
               {item.label}
             </button>
           ))}
         </div>
-        <div className="flex w-full flex-col items-center">
-          <div className="flex w-full items-center gap-x-2">
-            <span className="text-sm font-light text-gray-300">Available : </span>
-            <span className="text-sm text-gray-300">{avaliableBalance} KART</span>
-          </div>
-          <div className="w-full text-lg">
-            <div className="mt-2 flex items-center rounded-sm bg-transparent px-4 py-2 shadow-sm border border-purple-border">
-              <div className="relative flex grow items-stretch">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center gap-2">
-                  <Image
-                    alt="Kart Logo black"
-                    width={20}
-                    height={20}
-                    src="/images/logo.png"
-                  />
-                  <span className="text-gray-300 uppercase text-base">
-                    kart
-                  </span>
+        <div className={`flex w-full flex-col gap-5 ${selectedOption === StakingOptions[2].value && "h-48"}`}>
+          {
+            selectedOption !== StakingOptions[2].value &&
+            <>
+              <div className="flex w-full flex-col items-center">
+                <div className="flex w-full items-center gap-x-2">
+                  <span className="text-sm font-light text-gray-300">Available : </span>
+                  <span className="text-sm text-gray-300">{avaliableBalance} KART</span>
                 </div>
-                <Input
-                  type="number"
-                  className="block w-full bg-transparent py-2 pl-20 outline-none placeholder:text-lg placeholder:text-primary/50 sm:text-lg sm:leading-6"
-                  value={amount}
-                  onChange={handleAmountChange}
-                />
+                <div className="w-full text-lg">
+                  <div className="mt-2 flex items-center rounded-sm bg-transparent px-4 py-2 shadow-sm border border-purple-border">
+                    <div className="relative flex grow items-stretch">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center gap-2">
+                        <Image
+                          alt="Kart Logo black"
+                          width={20}
+                          height={20}
+                          src="/images/logo.png"
+                        />
+                        <span className="text-gray-300 uppercase text-base">
+                          kart
+                        </span>
+                      </div>
+                      <Input
+                        type="number"
+                        className="block w-full bg-transparent py-2 pl-20 outline-none placeholder:text-lg placeholder:text-primary/50 sm:text-lg sm:leading-6"
+                        value={amount}
+                        onChange={handleAmountChange}
+                      />
+                    </div>
+                    <button
+                      type="button"
+                      className="relative text-gray-300 inline-flex items-center gap-x-1.5 rounded bg-purple px-2 py-1.5 text-sm shadow-md"
+                      onClick={() => setAmount(avaliableBalance)}
+                    >
+                      MAX
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full flex-col gap-y-1 text-xs">
+                <div className="flex min-h-3 justify-between">
+                  {selectedOption === StakingOptions[1].value && (
+                    <>
+                      <div className="text-gray-300 text-sm">Unstaking Period</div>
+                      <div className="text-white text-sm">10 Days</div>
+                    </>
+                  )}
+                </div>
               </div>
               <button
-                type="button"
-                className="relative text-gray-300 inline-flex items-center gap-x-1.5 rounded bg-purple px-2 py-1.5 text-sm shadow-md"
-                onClick={() => setAmount(avaliableBalance)}
+                className={`flex flex-row gap-4 justify-center bg-purple border w-full rounded py-3 text-gray-300 transition-all duration-100 ease-in-out ${selectedOption === StakingOptions[0].value ? "bg-purple border-transparent" : "bg-transparent border-purple-border"}`}
+                onClick={
+                  selectedOption === StakingOptions[0].value
+                    ? handleStake
+                    : handleUnstake
+                }
               >
-                MAX
+                {selectedOption === StakingOptions[0].value ? "Stake" : "Unstake"}
               </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full flex-col gap-y-1 text-xs">
-          <div className="flex min-h-3 justify-between">
-            {selectedOption === StakingOptions[1].value && (
-              <>
-                <div className="text-gray-300 text-sm">Unstaking Period</div>
-                <div className="text-white text-sm">10 Days</div>
-              </>
-            )}
-          </div>
-        </div>
-        <button
-          className={`flex flex-row gap-4 justify-center bg-purple border w-full rounded py-3 text-gray-300 transition-all duration-100 ease-in-out ${selectedOption === StakingOptions[0].value ? "bg-purple border-transparent" : "bg-transparent border-purple-border"}`}
-          onClick={
-            selectedOption === StakingOptions[0].value
-              ? handleStake
-              : handleUnstake
+            </>
           }
-        >
-          {selectedOption === StakingOptions[0].value ? "Stake" : "Unstake"}
-        </button>
+        </div>
       </KartCard>
       <main className="mx-auto flex w-full max-w-7xl flex-col sm:mt-32 sm:hidden">
         <DataContainer />
